9b104a0c44a8990315ed0bda2b2a20ddecc985f9
Vesa Karvonen <vesa.a.j.k@gmail.com>
2019-04-13 11:49:17 +0300

    Simplified

--- provides/include/lax_v1/logical.hpp
+++ provides/include/lax_v1/logical.hpp
@@ -15,20 +15,12 @@ struct lax_v1::if_m<lax_v1::false_t, ThenExpr, ElseExprs...>
     : if_m<ElseExprs...> {};
 
 template <> struct lax_v1::and_m<> : true_t {};
-template <class... Exprs>
-struct lax_v1::and_m<lax_v1::false_t, Exprs...> : false_t {};
-template <class... Exprs>
-struct lax_v1::and_m<lax_v1::true_t, Exprs...> : and_m<Exprs...> {};
 template <class Expr, class... Exprs>
-struct lax_v1::and_m<Expr, Exprs...> : and_m<force_t<Expr>, Exprs...> {};
+struct lax_v1::and_m<Expr, Exprs...> : if_m<Expr, and_m<Exprs...>, false_t> {};
 
 template <class Expr>
 struct lax_v1::not_m : value_t<bool, !value_of_v<Expr>> {};
 
 template <> struct lax_v1::or_m<> : false_t {};
-template <class... Exprs>
-struct lax_v1::or_m<lax_v1::true_t, Exprs...> : true_t {};
-template <class... Exprs>
-struct lax_v1::or_m<lax_v1::false_t, Exprs...> : or_m<Exprs...> {};
 template <class Expr, class... Exprs>
-struct lax_v1::or_m<Expr, Exprs...> : or_m<force_t<Expr>, Exprs...> {};
+struct lax_v1::or_m<Expr, Exprs...> : if_m<Expr, true_t, or_m<Exprs...>> {};
